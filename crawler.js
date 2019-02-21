0c8f2e24f5aac64c7a7d9c44ea2cd7e1245a8e99
Sina Keshmiri <39638447+deepdownlow@users.noreply.github.com>
2019-02-21 15:02:31 -0800

    Update crawler.js

--- crawler.js
+++ crawler.js
@@ -51,89 +51,88 @@ const Process = () => {
     const fileStream = fs.readdirSync('C:/Users/TAG LISA/Desktop/FILE_DROP_OFF')
     let fileOne = fileStream[0] === 'FIFA' ? fileStream[0] : fileStream[1]
     let fileTwo = fileStream[0] !== 'DOMO' ? fileStream[0] : fileStream[1]
-    console.log(fileOne)
-    console.log(fileTwo)
-   //  const fifaCsvFilePath = `./FIFA_DROP_OFF/${ fifaFile }`
-   //  const domoFile = fs.readdirSync('./DOMO_DROP_OFF')[0]
-   //  const domoCsvFilePath = `./DOMO_DROP_OFF/${ domoFile }`
-   //  csv()
-   //  .fromFile(fifaCsvFilePath)
-   //  .then( fifaObj => {
-   //      csv()
-   //      .fromFile(domoCsvFilePath)
-   //      .then( domoObj => {
-   //          const FIFA_JSON = []
-   //          const DOMO_JSON = []
-   //          fifaObj.map(x => {
-   //              const OutLetId = x["OUTLET_ID"]
-   //              const CommisionType = x["COMMISSION_TYPE"]
-   //              const Order_ID = 'Order_ID'
-   //              const Product = 'Product'
-   //              const Product_index = x.COMMENT.indexOf(Product)
-   //              const Customer = 'Customer'
-   //              const Customer_index = x.COMMENT.indexOf(Customer)
-   //              const SalesRepId = 'Sales Rep ID'
-   //              const SalesRepId_index = x.COMMENT.indexOf(SalesRepId)
-   //              const SalesRepName = 'Sales Rep Name'
-   //              const SalesRepName_index = x.COMMENT.indexOf(SalesRepName)
-   //              const OrderValue = x.COMMENT.slice(Order_ID.length, Product_index)
-   //              const ProductValue = x.COMMENT.slice(Product_index + Product.length, Customer_index)
-   //              const CustomerName = x.COMMENT.slice(Customer_index + Customer.length, SalesRepId_index)
-   //              const RepId = x.COMMENT.slice(SalesRepId_index + SalesRepId.length, SalesRepName_index)
-   //              const RepName = x.COMMENT.slice(SalesRepName_index + SalesRepName.length, x.COMMENT.length - 1)
+    //ADD FILE PATH FOR PROCESSING
+    const fifaCsvFilePath = `./FIFA_DROP_OFF/${ fifaFile }`
+    const domoFile = fs.readdirSync('./DOMO_DROP_OFF')[0]
+    const domoCsvFilePath = `./DOMO_DROP_OFF/${ domoFile }`
+    csv()
+    .fromFile(fifaCsvFilePath)
+    .then( fifaObj => {
+        csv()
+        .fromFile(domoCsvFilePath)
+        .then( domoObj => {
+            const FIFA_JSON = []
+            const DOMO_JSON = []
+            fifaObj.map(x => {
+                const OutLetId = x["OUTLET_ID"]
+                const CommisionType = x["COMMISSION_TYPE"]
+                const Order_ID = 'Order_ID'
+                const Product = 'Product'
+                const Product_index = x.COMMENT.indexOf(Product)
+                const Customer = 'Customer'
+                const Customer_index = x.COMMENT.indexOf(Customer)
+                const SalesRepId = 'Sales Rep ID'
+                const SalesRepId_index = x.COMMENT.indexOf(SalesRepId)
+                const SalesRepName = 'Sales Rep Name'
+                const SalesRepName_index = x.COMMENT.indexOf(SalesRepName)
+                const OrderValue = x.COMMENT.slice(Order_ID.length, Product_index)
+                const ProductValue = x.COMMENT.slice(Product_index + Product.length, Customer_index)
+                const CustomerName = x.COMMENT.slice(Customer_index + Customer.length, SalesRepId_index)
+                const RepId = x.COMMENT.slice(SalesRepId_index + SalesRepId.length, SalesRepName_index)
+                const RepName = x.COMMENT.slice(SalesRepName_index + SalesRepName.length, x.COMMENT.length - 1)
        
-   //              const obj =
-   //              {
-   //                 OUTLET_ID: OutLetId,
-   //                 COMMISION_TYPE: CommisionType,
-   //                 ORDER_ID: OrderValue.slice(1).trim(),
-   //                 PRODUCT: ProductValue.slice(1).trim(),
-   //                 CUSTOMER: CustomerName.slice(1).trim(),
-   //                 SALES_REP_ID: RepId.slice(2).trim(),
-   //                 SALES_REP_NAME: RepName.slice(1).trim(),
-   //                 TOTAL_AP_CASH: '',
-   //                 TOTAL_AP_GST: ''
-   //              }
-   //              FIFA_JSON.push(obj)
-   //           })
-   //          domoObj.map(x => {
-   //              const OutLetId = x["NCCS_CHANNEL_OUTLET_ID"]
-   //              const CommisionType = x["COMMI_TYPE"]
-   //              const Order_ID = 'Order_ID'
-   //              const Product = 'Product'
-   //              const Product_index = x.COMMENT.indexOf(Product)
-   //              const Customer = 'Customer'
-   //              const Customer_index = x.COMMENT.indexOf(Customer)
-   //              const SalesRepId = 'Sales Rep ID'
-   //              const SalesRepId_index = x.COMMENT.indexOf(SalesRepId)
-   //              const SalesRepName = 'Sales Rep Name'
-   //              const SalesRepName_index = x.COMMENT.indexOf(SalesRepName)
-   //              const OrderValue = x.COMMENT.slice(Order_ID.length, Product_index)
-   //              const ProductValue = x.COMMENT.slice(Product_index + Product.length, Customer_index)
-   //              const CustomerName = x.COMMENT.slice(Customer_index + Customer.length, SalesRepId_index)
-   //              const RepId = x.COMMENT.slice(SalesRepId_index + SalesRepId.length, SalesRepName_index)
-   //              const RepName = x.COMMENT.slice(SalesRepName_index + SalesRepName.length, x.COMMENT.length - 1)
+                const obj =
+                {
+                   OUTLET_ID: OutLetId,
+                   COMMISION_TYPE: CommisionType,
+                   ORDER_ID: OrderValue.slice(1).trim(),
+                   PRODUCT: ProductValue.slice(1).trim(),
+                   CUSTOMER: CustomerName.slice(1).trim(),
+                   SALES_REP_ID: RepId.slice(2).trim(),
+                   SALES_REP_NAME: RepName.slice(1).trim(),
+                   TOTAL_AP_CASH: '',
+                   TOTAL_AP_GST: ''
+                }
+                FIFA_JSON.push(obj)
+             })
+            domoObj.map(x => {
+                const OutLetId = x["NCCS_CHANNEL_OUTLET_ID"]
+                const CommisionType = x["COMMI_TYPE"]
+                const Order_ID = 'Order_ID'
+                const Product = 'Product'
+                const Product_index = x.COMMENT.indexOf(Product)
+                const Customer = 'Customer'
+                const Customer_index = x.COMMENT.indexOf(Customer)
+                const SalesRepId = 'Sales Rep ID'
+                const SalesRepId_index = x.COMMENT.indexOf(SalesRepId)
+                const SalesRepName = 'Sales Rep Name'
+                const SalesRepName_index = x.COMMENT.indexOf(SalesRepName)
+                const OrderValue = x.COMMENT.slice(Order_ID.length, Product_index)
+                const ProductValue = x.COMMENT.slice(Product_index + Product.length, Customer_index)
+                const CustomerName = x.COMMENT.slice(Customer_index + Customer.length, SalesRepId_index)
+                const RepId = x.COMMENT.slice(SalesRepId_index + SalesRepId.length, SalesRepName_index)
+                const RepName = x.COMMENT.slice(SalesRepName_index + SalesRepName.length, x.COMMENT.length - 1)
        
-   //              const obj =
-   //              {
-   //                 OUTLET_ID: OutLetId,
-   //                 COMMISION_TYPE: CommisionType,
-   //                 ORDER_ID: OrderValue.slice(1).trim(),
-   //                 PRODUCT: ProductValue.slice(1).trim(),
-   //                 CUSTOMER: CustomerName.slice(1).trim(),
-   //                 SALES_REP_ID: RepId.slice(2).trim(),
-   //                 SALES_REP_NAME: RepName.slice(1).trim(),
-   //                 TOTAL_AP_CASH: '',
-   //                 TOTAL_AP_GST: ''
-   //              }
-   //              DOMO_JSON.push(obj)
-   //           })
-   //           WriteToCsv(FIFA_JSON, `MODIFIED_FIFA`)
-   //           WriteToCsv(DOMO_JSON, `MODIFIED_DOMO`)
-   //      })
-   //      .catch(err => console.log(err))
-   //  })
-   //  .catch(err => console.log(err))
+                const obj =
+                {
+                   OUTLET_ID: OutLetId,
+                   COMMISION_TYPE: CommisionType,
+                   ORDER_ID: OrderValue.slice(1).trim(),
+                   PRODUCT: ProductValue.slice(1).trim(),
+                   CUSTOMER: CustomerName.slice(1).trim(),
+                   SALES_REP_ID: RepId.slice(2).trim(),
+                   SALES_REP_NAME: RepName.slice(1).trim(),
+                   TOTAL_AP_CASH: '',
+                   TOTAL_AP_GST: ''
+                }
+                DOMO_JSON.push(obj)
+             })
+             WriteToCsv(FIFA_JSON, `MODIFIED_FIFA`)
+             WriteToCsv(DOMO_JSON, `MODIFIED_DOMO`)
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
  }
 
 Process()
